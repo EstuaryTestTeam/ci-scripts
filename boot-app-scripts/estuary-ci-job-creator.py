a2f4896b652c4021626bb892a2ed9e1c7091546b
AlamelaLiu <meili760628705@163.com>
2017-08-11 16:38:45 +0800

    add perf json

--- boot-app-scripts/estuary-ci-job-creator.py
+++ boot-app-scripts/estuary-ci-job-creator.py
@@ -115,6 +115,7 @@ dummy_ssh = {'device_type': 'dummy_ssh',
                             'qemu.json',
                             'lamp.json',
                             'lemp.json',
+                            'perf.json',
                             #'network_tests_basic.json',
                             'sysbench.json'],}
 
